c116c8c55880cba622fd7c8f459cc39f154348c5
FILLIN-RX <djeutchouruxel@gmail.com>
2025-11-25 09:37:44 +0100

    resolve routes

--- frontend/Stockly/src/components/LoginNav.vue
+++ frontend/Stockly/src/components/LoginNav.vue
@@ -186,7 +186,6 @@
         <div class="p-5">
 
           <ValidationButton :text="authStore.user?.type === 'admin' ? 'Retour à l\'admin' : 'Déconnexion'"
-            :icon="() => h('span', { class: 'material-symbols-outlined', style: 'font-size: 20px' }, 'logout')"
             size="large" :asyncClick="logoutEntreprise"
             class="w-full mt-3 flex justify-center gap-3 bg-gradient-to-r from-blue-600 to-blue-700 hover:from-blue-700 hover:to-blue-800 text-white px-5 py-3 rounded-2xl font-semibold shadow-md hover:shadow-lg transition-all" />
 
