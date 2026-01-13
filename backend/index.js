fa2b2b45a3062994d80bf8e607b9000dc07502a1
FILLIN-RX <djeutchouruxel@gmail.com>
2026-01-13 14:51:33 +0100

    redesigning the dashboard and navbar ui

--- backend/index.js
+++ backend/index.js
@@ -110,7 +110,8 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://iventello.vercel.app"], 
+    origin: ["http://localhost:5173"],
+    //"https://iventello.vercel.app"
     credentials: true,
   })
 );
